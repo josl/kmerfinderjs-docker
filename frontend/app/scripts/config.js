2e7b4b26aa385ababc7f5483084610ee8d5d876c
Jose L. Bellod Cisneros <bellod.cisneros@gmail.com>
2016-06-11 10:37:12 +0200

    Fastest KmerJS version added

--- frontend/app/scripts/config.js
+++ frontend/app/scripts/config.js
@@ -2,6 +2,6 @@
 
 angular.module('cgeUploaderApp.config', [])
 
-.constant('API', {status:'dev',url:'http://192.168.99.100:5000/'})
+.constant('API', {status:'prod',url:'http://compare.cbs.dtu.dk:5000/'})
 
 ;
