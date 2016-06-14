8f0e70d5251f2a77024da89037618521fe6be889
Jose L. Bellod Cisneros <bellod.cisneros@gmail.com>
2016-06-14 08:38:47 +0200

    Content-type header added

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(helmet());
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
