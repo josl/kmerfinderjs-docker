370838edeb3358871abf82e6f386b8f3b8ec1ca3
Jose L. Bellod Cisneros <bellod.cisneros@gmail.com>
2016-11-17 09:16:42 +0300

    New native KmerJS lib added

--- backend/kmerjs/native/kmerFinderServer.js
+++ backend/kmerjs/native/kmerFinderServer.js
@@ -67,7 +67,8 @@ function findKmersMatchesRedis(kmerMap, client, start) {
                     tScore: kmerCoverage,
                     uScore: 1,
                     lengths: template.lengths,
-                    ulength: template.ulengths,
+                    ulength: template.ulenght,
+                    // ulength: template.ulengths,
                     species: template.species,
                     kmers: {kmer : 1}
                 };
@@ -305,11 +306,13 @@ function sortKmerMatches(a, b) {
 
 function firstMatch(kmerObject, kmerMap) {
     let start = now();
-    kmerObject.redis.select(1);
+    kmerObject.redis.select(3);
+    // kmerObject.redis.select(1);
     return kmerObject.redis
         .hgetallAsync('Summary')
         .then(function (summary) {
-            kmerObject.redis.select(0);
+            kmerObject.redis.select(2);
+            // kmerObject.redis.select(0);
             kmerObject.summary = {
                 templates: +summary.templates,
                 totalLen: +summary.totalLen,
@@ -506,10 +509,15 @@ class KmerFinderServer extends KmerJS {
             method = 'standard', maxHits = 100) {
             super(fastq, preffix, length, step, coverage, progress, 'node');
             this.redis = redis.createClient({
-                host: url.substring(8, 22),
-                port: +url.substring(23, 28),
-                url: url
+                host: '127.0.0.1',
+                port: 6379,
+                url: 'redis://127.0.0.1:6379'
             });
+            // this.redis = redis.createClient({
+            //     host: url.substring(8, 22),
+            //     port: +url.substring(23, 28),
+            //     url: url
+            // });
             this.redis.on('error', function (err) {
                 throw new Error(err);
             });
--- scripts/kmersToRedis.py
+++ scripts/kmersToRedis.py
@@ -3,14 +3,16 @@
 import json
 import redis
 
-r = redis.StrictRedis(host='192.168.99.100', port=6379, db=0)
+r = redis.StrictRedis(db=2)
+# r = redis.StrictRedis(host='192.168.99.100', port=6379, db=2)
 print 'loading data...'
-with open('database/jsonindex2.json') as data_file:
+with open('/Users/cisneror/code/genomic-git/kmerjs/test_data/myDB5_redis.json') as data_file:
     data = json.load(data_file)
 print 'data loaded!'
 for entry in data:
     if entry['kmer'] != '':
         for template in entry['templates']:
+            print entry['kmer'], template['sequence']
             r.rpush(entry['kmer'], json.dumps(template))
         # try:
         #     r.hmset(entry['kmer'], entry['templates'])
